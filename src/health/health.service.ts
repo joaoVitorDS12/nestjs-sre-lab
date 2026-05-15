5c394b6235354a37a7cafe7a8f8fd57ae472efd8
Joao Vitor dos Santos <jvitorgit@gmail.com>
2026-05-15 18:54:21 -0300

    feat: create startup probe to check health

--- src/health/health.service.ts
+++ src/health/health.service.ts
@@ -3,10 +3,12 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class HealthService {
   checkHealth(): string {
+    console.log('Health is okay');
     return 'OK!';
   }
 
   checkReady(): string {
+    console.log('Ready is okay');
     return 'OK!';
   }
 }
