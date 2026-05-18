d8216bfd54062e15c08526bae09fb6c5e5f64db0
Joao Vitor dos Santos <jvitorgit@gmail.com>
2026-05-18 00:20:00 -0300

    refactor: make the application health again after tests

--- src/health/health.service.ts
+++ src/health/health.service.ts
@@ -2,16 +2,13 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class HealthService {
-  checkHealth(): boolean {
-    console.log('Health is okay');
-    if (new Date().getMilliseconds() % 2 === 0) {
-      return true;
-    }
-    return false;
+  checkHealth(): string {
+    console.log('Checking application heath');
+    return 'OK!';
   }
 
   checkReady(): string {
-    console.log('Ready is okay');
+    console.log('Checking application readiness');
     return 'OK!';
   }
 }
